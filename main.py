9bc6978eede23bef1cd19112d494ab5805c25dba
Pythom1234 <149593207+Pythom1234@users.noreply.github.com>
2024-04-16 12:00:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,57 +1,110 @@
-import os
 import tkinter
 import glob
+import os
 
-if os.name == 'posix':
-    try:
-        os.chdir(os.path.expanduser('~')+'/.pydownload')
-    except:
-        os.system('mkdir ~/.pydownload')
-        os.system('python3 -m venv ~/.pydownload/venv')
-        os.system(f'source ~/.pydownload/venv/bin/activate; python3 -m pip install --upgrade pip')
-        os.chdir(os.path.expanduser('~')+'/.pydownload')
+if not os.name == 'posix':
+    raise EnvironmentError('this program is only for linux')
 
-    os.system('git clone -qb main https://github.com/Pythom1234/projects.git')
-    with open('projects/includes') as s:
-        includes = s.read().split('\n')
-    os.system('rm -rf projects')
+def download(name):
+    with open('includes',mode='r') as s:
+        dirs = s.read().split('\n')
+    with open('includes',mode='a') as s:
+        if not name in dirs:
+            s.write('\n'+name)
+    name = name.split('==')[0]
+    os.system(f'git clone -qb {name} https://github.com/Pythom1234/projects.git')
+    os.system('rm -rf projects/.git')
+    os.system(f'mv ~/.pydownload/projects ~/.pydownload/{name}')
+    with open(name+'/req') as s:
+        req = s.read()
+    os.system(f'source ~/.pydownload/venv/bin/activate; python3 -m pip install {req}')
+    prepare()
 
+def run(name):
+    with open(name+'/run') as s:
+        run = s.read()
+    os.system(f"""konsole --hold -e 'bash -c "source ~/.pydownload/venv/bin/activate; python3 ~/.pydownload/{run}"'""")
 
-    def b2():
-        global b4
-        os.system(f'rm -rf {l1.get(l1.curselection()[0])}')
-        os.system(f'git clone -qb {l1.get(l1.curselection()[0])} https://github.com/Pythom1234/projects.git')
-        os.system('rm -rf projects/.git')
-        os.system(f'mv ~/.pydownload/projects ~/.pydownload/{l1.get(l1.curselection()[0])}')
-        with open(l1.get(l1.curselection()[0])+'/req') as s:
-            req = s.read()
-        os.system(f'source ~/.pydownload/venv/bin/activate; python3 -m pip install {req}')
-        b4.grid(row=3,column=2)
+def delete():
+    os.chdir(os.path.expanduser('~'))
+    os.system('rm -rf .pydownload')
+    exit()
 
-    def b3():
-        with open(l1.get(l1.curselection()[0])+'/run') as s:
-            run = s.read()
-        os.system(f"""konsole --hold -e 'bash -c "source ~/.pydownload/venv/bin/activate; python3 {os.path.expanduser('~')}/.pydownload/{run}"'""")
-
-    def c1(c):
-        global b4
-        if os.path.expanduser('~') + '/.pydownload/' + l1.get(l1.curselection()[0]) in glob.glob(os.path.expanduser('~')+'/.pydownload/*'):
-            b4.grid(row=3,column=2)
-        else:
-            #b4.destroy()
-            b4.grid_forget()
-
-    b4 = None
-
-    root = tkinter.Tk()
-    tkinter.Label(text='Nainstalovat: ').grid(row=2,column=0)
-    l1 = tkinter.Listbox()
+def install_all():
     for i in includes:
         if not i == '':
-            l1.insert('end',i)
-    b1 = tkinter.Button(text='Nainstalovat',command=b2)
-    b4 = tkinter.Button(text='Spustit',command=b3)
-    b1.grid(row=2,column=2)
-    l1.grid(row=2,column=1)
-    l1.bind('<ButtonRelease-1>', c1)
-    root.mainloop()
+            download(i)
+
+def get_func_for_download(name):
+    return lambda: download(name)
+
+def get_func_for_run(name):
+    return lambda: run(name)
+
+def prepare():
+    global tk_downloads, tk_labels, tk_runs
+    for i in tk_downloads:
+        i.destroy()
+    for i in tk_labels:
+        i.destroy()
+    for i in tk_runs:
+        i.destroy()
+    tk_downloads = []
+    tk_labels = []
+    tk_runs = []
+    with open('includes') as s:
+        dirs = s.read()
+    for i in includes:
+        if not i == '':
+            tk_labels.append(tkinter.Label(text=i.split('==')[0]))
+            tk_labels[-1].grid(row=includes.index(i),column=0)
+            if not i.split('==')[0] in dirs:
+                tk_downloads.append(tkinter.Button(text='Stáhnout',command=get_func_for_download(i)))
+                tk_downloads[-1].grid(row=includes.index(i),column=1)
+            elif not i in dirs:
+                tk_downloads.append(tkinter.Button(text='Aktualizovat',command=get_func_for_download(i)))
+                tk_downloads[-1].grid(row=includes.index(i),column=1)
+            else:
+                tk_downloads.append(tkinter.Button(text='Aktuální',state='disabled'))
+                tk_downloads[-1].grid(row=includes.index(i),column=1)
+                tk_runs.append(tkinter.Button(text='Spustit',command=get_func_for_run(i.split('==')[0])))
+                tk_runs[-1].grid(row=includes.index(i),column=2)
+
+try:
+    os.chdir(os.path.expanduser('~')+'/.pydownload')
+except:
+    os.system('mkdir ~/.pydownload')
+    os.system('python3 -m venv ~/.pydownload/venv')
+    os.system(f'source ~/.pydownload/venv/bin/activate; python3 -m pip install --upgrade pip')
+    os.chdir(os.path.expanduser('~')+'/.pydownload')
+
+
+os.chdir(os.path.expanduser('~')+'/.pydownload')
+
+os.system('git clone -qb main https://github.com/Pythom1234/projects.git')
+with open('projects/includes') as s:
+    includes = s.read().split('\n')
+os.system('rm -rf projects')
+
+if not 'includes' in glob.glob('*'):
+    with open('includes',mode='w'):
+        pass
+
+tk_labels = []
+tk_downloads = []
+tk_runs = []
+
+root = tkinter.Tk()
+
+menu_bar = tkinter.Menu(root)
+menu = tkinter.Menu(menu_bar, tearoff=0)
+menu.add_command(label='Vyčistit paměť (automaticky ukončí)',command=delete)
+menu.add_command(label='Nainstalovat vše (pomalé)',command=install_all)
+menu_bar.add_cascade(label='Možnosti', menu=menu)
+
+root.config(menu=menu_bar)
+
+prepare()
+
+root.mainloop()
+
